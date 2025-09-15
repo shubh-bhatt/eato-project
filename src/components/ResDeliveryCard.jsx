a5a93ec75bbd10edb1a5a12e4ca5cb3523253d48
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-09-15 13:13:49 +0530

    hero section completed

--- src/components/ResDeliveryCard.jsx
+++ src/components/ResDeliveryCard.jsx
@@ -1,7 +1,8 @@
+import { RATING_SVG } from "../utils/constants";
+
 const ResDeliveryCard = (props) => {
   // console.log(props.res);
   const { name, rating, delivery_time, area, image_url } = props.res;
-  console.log(props.res);
 
   return (
     <div className="res-delivery-card">
@@ -10,6 +11,21 @@ const ResDeliveryCard = (props) => {
       </div>
       <div className="res-delivery-details">
         <div className="res-delivery-name">{name}</div>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ marginRight: "4px" }}>{RATING_SVG}</span>
+          <span className="res-delivery-rating">{rating} rating </span>
+          <span className="dot" />
+          <span className="res-delivery-time"> {delivery_time}</span>
+        </div>
+
+        <div className="res-delivery-cuisines">{name}</div>
+        <div className="res-delivery-area">{area}</div>
       </div>
     </div>
   );
