9dbe2a592037aec9a9c39e8cd7d7fcf0a16d5f04
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-09-16 15:44:39 +0530

    data added

--- src/components/ResDeliveryContainer.jsx
+++ src/components/ResDeliveryContainer.jsx
@@ -1,7 +1,11 @@
 import ResDeliveryCard from "./ResDeliveryCard";
 import { restaurantDelivery } from "../../public/data/restaurantDelivery";
+import { useEffect, useState } from "react";
 
 const ResDeliveryContainer = () => {
+  const [restaurantDelivery, setRestaurantDelivery] = useState([]);
+
+  useEffect(() => {}, []);
   return (
     <div className="res-delivery-container">
       <div className="res-delivery-title">
