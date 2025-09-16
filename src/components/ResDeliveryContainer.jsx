08a38b482c6b5f21fd79b5d0ab6254042f8b5824
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-09-16 15:47:04 +0530

    fetch added

--- src/components/ResDeliveryContainer.jsx
+++ src/components/ResDeliveryContainer.jsx
@@ -5,7 +5,22 @@ import { useEffect, useState } from "react";
 const ResDeliveryContainer = () => {
   const [restaurantDelivery, setRestaurantDelivery] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          "https://eato-project-six.vercel.app/data/restaurantDelivery.json"
+        );
+        const data = await res.json();
+        setRestaurantDelivery(data);
+      } catch (err) {
+        console.log("Failed to fetch Restaurant Delivery data:", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="res-delivery-container">
       <div className="res-delivery-title">
