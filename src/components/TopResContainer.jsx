3d071c02dde294bde74ccddb76e820c475f113bb
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-09-16 15:33:17 +0530

    data fetch

--- src/components/TopResContainer.jsx
+++ src/components/TopResContainer.jsx
@@ -1,7 +1,24 @@
 import TopResCard from "./TopResCard";
-import { topResData } from "../../public/data/topResData";
+import { useEffect, useState } from "react";
 
 const TopResContainer = () => {
+  const [topResData, setTopResData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          "https://eato-project-six.vercel.app/data/topResData.json"
+        );
+        const data = await res.json();
+        setTopResData(data);
+      } catch (err) {
+        console.error("Failed to fetch restaurants:", err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="top-res-container">
       <div className="top-res-title">Top restaurant chains in India</div>
